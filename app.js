60ee3fc4088f9a06489123976daf3459b8a31877
Gerald <Fareltripp@gmail.com>
2019-11-05 12:40:56 +0100

    Editing the app file

--- app.js
+++ app.js
@@ -9,3 +9,24 @@ const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
+
+//CART
+
+let cart = [];
+
+//GETTING THE PRODUCTS
+
+class Products {}
+
+//DISPLAY PRODUCTS
+
+class UI {}
+
+//LOCAL STORAGE
+
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+});
