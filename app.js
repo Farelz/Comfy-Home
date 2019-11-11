add3e902a7a58b981b34e3cd930df4a5a9017043
Gerald <Fareltripp@gmail.com>
2019-11-11 12:25:55 +0100

    Updating the app file

--- app.js
+++ app.js
@@ -61,6 +61,21 @@ class UI {
     });
     productsDOM.innerHTML = result;
   }
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id === id);
+      if(inCart){
+        button.innerText = 'In Cart';
+        button.disabled = true;
+      } else {
+        button.addEventListener('click', event => {
+          console.log(event);
+        })
+      }
+    });
+  }
 }
 
 //LOCAL STORAGE
